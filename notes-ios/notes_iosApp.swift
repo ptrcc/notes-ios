606ddce94c92c29acde9e725947d6e6de48b1191
Patrick Arbeiter <arbeiterpatrick@gmail.com>
2023-10-21 11:56:00 +0200

    initial commit

---
+++ notes-ios/notes_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  notes_iosApp.swift
+//  notes-ios
+//
+//  Created by Patrick Arbeiter on 21.10.23.
+//
+
+import SwiftUI
+
+@main
+struct notes_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
