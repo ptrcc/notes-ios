bbefafa79a56dac157c88aa04cac8b1f01d0aa19
Patrick Arbeiter <arbeiterpatrick@gmail.com>
2023-10-22 11:11:21 +0200

    Added Editable Note List

---
+++ notes-ios/EditableList.swift
@@ -0,0 +1,18 @@
+//
+//  EditableList.swift
+//  notes-ios
+//
+//  Created by Patrick Arbeiter on 22.10.23.
+//
+
+import SwiftUI
+
+struct EditableList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditableList()
+}
